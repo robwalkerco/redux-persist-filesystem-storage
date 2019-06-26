a5a83385d3c40cd4a43ce04b3ec9391eef17223a
Rob Walker <rob@papertrail.io>
2019-06-26 08:58:24 +0100

    Normalise style

--- index.js
+++ index.js
@@ -1,141 +1,153 @@
 /**
-* @flow
-*/
+ * @flow
+ */
 
-import RNFetchBlob from 'rn-fetch-blob'
+import RNFetchBlob from "rn-fetch-blob";
 
 const createStoragePathIfNeeded = path =>
-  RNFetchBlob.fs.exists(path).then(exists => {
-    return exists
-      ? new Promise(resolve => resolve(true))
-      : RNFetchBlob.fs.mkdir(path);
-  });
-const onStorageReadyFactory = (storagePath) => {
-  return (func) => {
-    const storage = createStoragePathIfNeeded(storagePath);
-    return (...args) => storage.then(() => func(...args));
-  }
+  RNFetchBlob.fs
+    .exists(path)
+    .then(exists =>
+      exists
+        ? new Promise(resolve => resolve(true))
+        : RNFetchBlob.fs.mkdir(path)
+    );
+
+const onStorageReadyFactory = storagePath => func => {
+  const storage = createStoragePathIfNeeded(storagePath);
+
+  return (...args) => storage.then(() => func(...args));
 };
 
-const defaultStoragePath = `${RNFetchBlob.fs.dirs.DocumentDir}/persistStore`
+const defaultStoragePath = `${RNFetchBlob.fs.dirs.DocumentDir}/persistStore`;
 
 let onStorageReady = onStorageReadyFactory(defaultStoragePath);
 let options = {
   storagePath: defaultStoragePath,
-  encoding: 'utf8',
-  toFileName: (name: string) => name.split(':').join('-'),
-  fromFileName: (name: string) => name.split('-').join(':'),
+  encoding: "utf8",
+  toFileName: (name: string) => name.split(":").join("-"),
+  fromFileName: (name: string) => name.split("-").join(":")
 };
 
-const pathForKey = (key: string) => `${options.storagePath}/${options.toFileName(key)}`
+const pathForKey = (key: string) =>
+  `${options.storagePath}/${options.toFileName(key)}`;
 
 const FilesystemStorage = {
-  config: (
-    customOptions: Object,
-  ) => {
+  config: (customOptions: Object) => {
     options = {
       ...options,
-      ...customOptions,
-    }
+      ...customOptions
+    };
     onStorageReady = onStorageReadyFactory(options.storagePath);
   },
 
-  setItem: (
-    key: string,
-    value: string,
-    callback?: (error: ?Error) => void,
-  ) =>
-    RNFetchBlob.fs.writeFile(pathForKey(key), value, options.encoding)
+  setItem: (key: string, value: string, callback?: (error: ?Error) => void) =>
+    RNFetchBlob.fs
+      .writeFile(pathForKey(key), value, options.encoding)
       .then(() => callback && callback())
       .catch(error => callback && callback(error)),
 
-  getItem: onStorageReady((
-    key: string,
-    callback: (error: ?Error, result: ?string) => void
-  ) => {
-    const filePath = pathForKey(options.toFileName(key));
+  getItem: onStorageReady(
+    (key: string, callback: (error: ?Error, result: ?string) => void) => {
+      const filePath = pathForKey(options.toFileName(key));
 
-    return RNFetchBlob.fs.readFile(filePath, options.encoding)
-      .then(data => {
-        callback && callback(null, data)
-        if (!callback) return data
-      })
-      .catch(error => {
-        return RNFetchBlob.fs
-          .exists(filePath)
-          .then(exists => {
-            if (exists) {
-              // The error is not related to the existence of the file
+      return RNFetchBlob.fs
+        .readFile(filePath, options.encoding)
+        .then(data => {
+          callback && callback(null, data);
+          if (!callback) {
+            return data;
+          }
+        })
+        .catch(error =>
+          RNFetchBlob.fs
+            .exists(filePath)
+            .then(exists => {
+              if (exists) {
+                // The error is not related to the existence of the file
+                callback && callback(error);
+                if (!callback) {
+                  throw error;
+                }
+              }
+
+              return "";
+            })
+            .catch(() => {
+              // We throw the original error
               callback && callback(error);
-              if (!callback) throw error;
-            }
+              if (!callback) {
+                throw error;
+              }
+            })
+        );
+    }
+  ),
 
-            return '';
-          })
-          .catch(() => {
-            // We throw the original error
-            callback && callback(error);
-            if (!callback) throw error;
-          });
-      });
-  }),
-
-  removeItem: (
-    key: string,
-    callback: (error: ?Error) => void,
-  ) =>
-    RNFetchBlob.fs.unlink(pathForKey(options.toFileName(key)))
+  removeItem: (key: string, callback: (error: ?Error) => void) =>
+    RNFetchBlob.fs
+      .unlink(pathForKey(options.toFileName(key)))
       .then(() => callback && callback())
       .catch(error => {
-        callback && callback(error)
-        if (!callback) throw error
+        callback && callback(error);
+        if (!callback) {
+          throw error;
+        }
       }),
 
-  getAllKeys: (
-    callback: (error: ?Error, keys: ?Array<string>) => void,
-  ) =>
-    RNFetchBlob.fs.exists(options.storagePath)
+  getAllKeys: (callback: (error: ?Error, keys: ?Array<string>) => void) =>
+    RNFetchBlob.fs
+      .exists(options.storagePath)
       .then(exists =>
         exists ? true : RNFetchBlob.fs.mkdir(options.storagePath)
       )
       .then(() =>
-        RNFetchBlob.fs.ls(options.storagePath)
+        RNFetchBlob.fs
+          .ls(options.storagePath)
           .then(files => files.map(file => options.fromFileName(file)))
           .then(files => {
-            callback && callback(null, files)
-            if (!callback) return files
+            callback && callback(null, files);
+            if (!callback) {
+              return files;
+            }
           })
       )
       .catch(error => {
-        callback && callback(error)
-        if (!callback) throw error
-      }),
-}
+        callback && callback(error);
+        if (!callback) {
+          throw error;
+        }
+      })
+};
 
-FilesystemStorage.clear = (
-  callback: (error: ?Error) => void,
-) =>
+FilesystemStorage.clear = (callback: (error: ?Error) => void) =>
   FilesystemStorage.getAllKeys((error, keys) => {
-    if (error) throw error
+    if (error) throw error;
 
     if (Array.isArray(keys) && keys.length) {
-      const removedKeys = []
+      const removedKeys = [];
 
       keys.forEach(key => {
         FilesystemStorage.removeItem(key, (error: ?Error) => {
-          removedKeys.push(key)
-          if (error && callback) callback(error, false)
-          if (removedKeys.length === keys.length && callback) callback(null, true)
-        })
-      })
-      return true
+          removedKeys.push(key);
+          if (error && callback) {
+            callback(error, false);
+          }
+
+          if (removedKeys.length === keys.length && callback)
+            callback(null, true);
+        });
+      });
+      return true;
     }
 
-    callback && callback(null, false)
-    return false
+    callback && callback(null, false);
+    return false;
   }).catch(error => {
-    callback && callback(error)
-    if (!callback) throw error
-  })
+    callback && callback(error);
+    if (!callback) {
+      throw error;
+    }
+  });
 
-export default FilesystemStorage
+export default FilesystemStorage;
